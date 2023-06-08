099c3bebee32f076413ea28bd32a1fbd7b3ab38d
Padmanabhan_S <g.s.r.padmanabhan@gmail.com>
2023-06-08 17:44:55 +0530

    authorization

--- src/main/java/com/ibm/todoapp/controllers/TodoController.java
+++ src/main/java/com/ibm/todoapp/controllers/TodoController.java
@@ -1,11 +1,7 @@
 package com.ibm.todoapp.controllers;
 
 import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
 import javax.validation.Valid;
 
@@ -15,25 +11,19 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ibm.todoapp.dto.TodoDTO;
 import com.ibm.todoapp.exceptions.TodoNotFoundException;
 import com.ibm.todoapp.exceptions.UnAuthorizedException;
-import com.ibm.todoapp.models.Role;
 import com.ibm.todoapp.models.Todo;
-import com.ibm.todoapp.models.User;
 import com.ibm.todoapp.services.ITodoService;
 
 @RestController
@@ -54,10 +44,17 @@ public class TodoController {
 		if (role.equals("ROLE_Admin")) {
 			List<Todo> todos = todoSvc.getAllTodos();
 			todoDTO = todoSvc.TodotoTodoDTO(todos);
+			if (todos.isEmpty()) {
+				throw new TodoNotFoundException("Todo Not Found");
+			}
 		} else if (role.equals("ROLE_User")) {
 			String username = authentication.getName();
 			List<Todo> todos = todoSvc.getTodoByCreatedBy(username);
-			todoDTO = todoSvc.TodotoTodoDTO(todos);
+			if (todos != null) {
+				todoDTO = todoSvc.TodotoTodoDTO(todos);
+			} else {
+				throw new UnAuthorizedException("User is not authorized to access this todo item.");
+			}
 		}
 		return todoDTO;
 
@@ -81,9 +78,10 @@ public class TodoController {
 		} else if (role.equals("ROLE_User")) {
 			String username = authentication.getName();
 			Todo todos = todoSvc.getByIdAndCreatedBy(id, username);
-			todoDTO = todoSvc.TodotoTodoDTO(todos);
-			if (todos == null) {
-				throw new TodoNotFoundException("Todo Not Found");
+			if (todos != null) {
+				todoDTO = todoSvc.TodotoTodoDTO(todos);
+			} else {
+				throw new UnAuthorizedException("User is not authorized to access this todo item.");
 			}
 		}
 
@@ -93,7 +91,7 @@ public class TodoController {
 
 	@GetMapping("/title/{title}")
 	@PreAuthorize("hasAnyRole('User','Admin')")
-	public List<TodoDTO> getByTodoTitle(@PathVariable String title) throws TodoNotFoundException{
+	public List<TodoDTO> getByTodoTitle(@PathVariable String title) throws TodoNotFoundException {
 		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 		String role = authentication.getAuthorities().iterator().next().getAuthority();
 
@@ -108,9 +106,10 @@ public class TodoController {
 		} else if (role.equals("ROLE_User")) {
 			String username = authentication.getName();
 			List<Todo> todos = todoSvc.getByTitleAndCreatedBy(title, username);
-			todoDTO = todoSvc.TodotoTodoDTO(todos);
-			if (todos.isEmpty()) {
-				throw new TodoNotFoundException("Todo Not Found");
+			if (!todos.isEmpty()) {
+				todoDTO = todoSvc.TodotoTodoDTO(todos);
+			} else {
+				throw new UnAuthorizedException("User is not authorized to access this todo item.");
 			}
 		}
 
@@ -121,7 +120,7 @@ public class TodoController {
 	@PostMapping()
 	@PreAuthorize("hasAnyRole('User','Admin')")
 	public ResponseEntity<TodoDTO> createTodo(@Valid @RequestBody TodoDTO todoDTO) { // modelbinding ? spring validation
-		Todo newTodo = todoSvc.TodoDTOtoTodo(todoDTO);																	// framework
+		Todo newTodo = todoSvc.TodoDTOtoTodo(todoDTO); // framework
 		Todo newTodo1 = todoSvc.addTodo(newTodo);
 		TodoDTO todoDTO2 = todoSvc.TodotoTodoDTO(newTodo1);
 		return new ResponseEntity<TodoDTO>(todoDTO2, HttpStatus.CREATED);
@@ -130,38 +129,36 @@ public class TodoController {
 	@PutMapping("/{id}")
 	@PreAuthorize("hasAnyRole('User','Admin')")
 	public ResponseEntity<TodoDTO> updateTodo(@PathVariable Integer id, @Valid @RequestBody TodoDTO todoDTO) { // modelbinding
-																											// ?
+																												// ?
 		// spring validation
 		// framework
 		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 		String role = authentication.getAuthorities().iterator().next().getAuthority();
 
 		Todo newTodo = todoSvc.TodoDTOtoTodo(todoDTO);
-		
+
 		if (role.equals("ROLE_Admin")) {
 			Todo todos = todoSvc.getById(id);
-			if (todos != null) {
 			Todo todos1 = todoSvc.updateTodo(id, newTodo);
 			TodoDTO todoDTO1 = todoSvc.TodotoTodoDTO(todos1);
-				return new ResponseEntity<TodoDTO>(todoDTO1, HttpStatus.OK);
-			} else {
-				throw new UnAuthorizedException("Sorry,your request could not be processed");
+			if(todos==null) {
+				throw new TodoNotFoundException("Todo Not Found");
 			}
+			return new ResponseEntity<TodoDTO>(todoDTO1, HttpStatus.OK);
+			
 
 		} else if (role.equals("ROLE_User")) {
 			String username = authentication.getName();
 			Todo todos = todoSvc.getByIdAndCreatedBy(id, username);
 			if (todos != null) {
-			Todo todos1 = todoSvc.updateTodo(id, newTodo);
-			TodoDTO todoDTO1 = todoSvc.TodotoTodoDTO(todos1);
+				Todo todos1 = todoSvc.updateTodo(id, newTodo);
+				TodoDTO todoDTO1 = todoSvc.TodotoTodoDTO(todos1);
 				return new ResponseEntity<TodoDTO>(todoDTO1, HttpStatus.OK);
 			} else {
-				throw new UnAuthorizedException("Sorry,your request could not be processed");
+				throw new UnAuthorizedException("User is not authorized to access this todo item.");
 			}
-		} else {
-			throw new UnAuthorizedException("Sorry,your request could not be processed");
-		}
-
+		} 
+		return null;
 	}
 
 	@DeleteMapping("/{id}")
@@ -185,24 +182,9 @@ public class TodoController {
 			if (todos != null) {
 				todoSvc.deleteByIdAndCreatedBy(id, username);
 			} else {
-				throw new UnAuthorizedException("Sorry,your request could not be processed");
+				throw new UnAuthorizedException("User is not authorized to access this todo item.");
 			}
-		} else {
-			throw new UnAuthorizedException("Sorry,your request could not be processed");
-		}
+		} 
 		return null;
-	}
-
-	// MethodArgumentNotValidException
-	@ResponseStatus(HttpStatus.BAD_REQUEST)
-	@ExceptionHandler(MethodArgumentNotValidException.class)
-	public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
-		Map<String, String> errors = new HashMap<>();
-		ex.getBindingResult().getAllErrors().forEach((error) -> {
-			String fieldName = ((FieldError) error).getField();
-			String errorMessage = error.getDefaultMessage();
-			errors.put(fieldName, errorMessage);
-		});
-		return errors;
 	}
 }
